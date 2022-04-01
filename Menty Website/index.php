00fb0859f13852a5576ee9b37cbaf014026aacf2
Ananda Rauf <anandarauf02@gmail.com>
2022-04-02 04:53:24 +0700

    Menty profile website
    Menty website include virtual assistant dev by Hars Sharma and disease detection dev by Ananda Rauf Maududi.

---
+++ Menty Website/index.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+
+<script>
+
+  alert("Welcome to Menty Personal Assistant");
+
+</script>
+
+<meta name="author" content="Ananda Rauf Maududi,Hars Sharma"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta http-equiv="Content-Type" content="HTML,CSS,JAVASCRIPT; charset=utf-8"/>
+<meta name="keywords" content="html,css,js" />
+<meta name=keywords" content="Menty,menty,mently healhty,Mently Healthy,Disease Detection,Personal Assistant, personal assistant,disease detection,disease"/>
+<meta name="description" content="Menty Mently Healthy projects Personal Assistant and Disease Detection" />
+<link rel="canonical" href="https://menty.000webhostapp.com/menty"/>
+<link rel="stylesheet" href="css/index.css" type="text/css"/>
+<script src="js/index.js"></script>
+<link rel="icon" href="asset/favicon.ico" type="image/x-icon"/>
+</head>
+<title>Menty(Mently Healthy) - Personal Asssistant and Disease Detection</title>
+<body>
+<div class="scroll-button">
+    <a href="#home"><i class="fas fa-arrow-up"></i></a>
+  </div>
+  <!-- navgaition menu -->
+  <nav>
+    <div class="navbar">
+      <div class="logo"><a href="https://menty.000webhostapp.com/menty">Menty</a></div>
+      <ul class="menu">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <div class="cancel-btn">
+            <i class="fas fa-times"></i>
+          </div>
+      </ul>
+      <div class="media-icons">
+        <a href="https://www.linkedin.com/in/ananda-rauf-maududi"><i class="fa-brands fa-linkedin">Linkedin Ananda Rauf Maududi</i></a>
+        <a href="https://www.linkedin.com/in/"><i class="fa-brands fa-linkedin">Linkedin Ananda Rauf Maududi</i></a>
+       
+      </div>
+    </div>
+    <div class="menu-btn">
+      <i class="fas fa-bars"></i>
+    </div>
+  </nav>
+
+
+ <section class="home" id="home">
+   <div class="home-content">
+     <div class="text">
+       <div class="text-one">Hello,</div>
+       <div class="text-two">I'm Menty</div>
+       <div class="text-three">AI Personal Assistant</div>
+       <div class="text-four">My Developer is Hars Sharma and Ananda Rauf Maududi</div>
+       <div class="text-five">Can I help you?</div>
+     </div>
+     <div class="button">
+       <a href="personal-assistant/index.html"><button class="personal-as">Start Personal Assistant</button></a>
+       <a href="medison/index.html"><button class="disease-dt">Start Disease Detection</button></a>
+     </div>
+   </div>
+ </section>
+
+
+<section class="about" id="about">
+  <div class="content">
+    <div class="title"><span>About Me</span></div>
+  <div class="about-details">
+    <div class="left">
+      <img src="asset/menty.png" alt="Menty AI Personal Assistant">
+    </div>
+    <div class="right">
+      <div class="topic">Menty(Mently Healthy)</div>
+      <p>Menty is projects Google Challenge Solution for health being good, Menty started developed on 19 March - 22 March 2022.Menty Developer is Hars Sharma and Ananda Rauf Maududi.Ananda Rauf Maududi as Leader and Hars Sharma as CTO/Technical</p>
+      <div class="button">
+        <a href="https://www.linkedin.com/in/ananda-rauf-maududi"<button>Follow and Connect Linkedin Ananda Rauf Maududi</button>
+        <br></a></br>
+        <a href="https://www.linkedin.com/in/ananda-rauf-maududi"<button>Follow and Connect Linkedin Hars Sharma</button></a>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+
+</body>
+</html>
+
+
+
---
+++ Menty Website/personal-assistant/app1.js
@@ -0,0 +1,88 @@
+const dialogflow = require("@google-cloud/dialogflow");
+const uuid = require("uuid");
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 5000;
+const app = express();
+app.use(express.json({ limit: "10000mb" }));
+
+// A unique identifier for the given session
+const sessionId = uuid.v4();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post("/send-msg", (req, res) => {
+  console.log(req.body.data);
+  runSample(req.body.data).then((message) => {
+    res.send({ Reply: message });
+  });
+});
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function runSample(msg, projectId = "chatbotdemoexperience-ldce") {
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    keyFilename:
+      "D:/GoogleSolChallenge/chatbotdemoexperience-ldce-91c103f93191.json",
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: msg,
+        // The language used by the client (en-US)
+        languageCode: "en-US",
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log("  No intent matched.");
+  }
+  return [result.fulfillmentText, result.intent.displayName];
+}
+
+app.listen(port, () => {
+  console.log("Running on Port " + port);
+});
+//<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://dumetschool.com"});
+//]]>
